a8f375d924348e1388d0b8b7782c80b796cf0d24
Daisy <yuanning0212@yeah.net>
2025-05-05 14:22:54 +0800

    有点小问题还没解决

--- books-manage-sys/src/main/java/cn/kmbeast/service/impl/BookOrderHistoryServiceImpl.java
+++ books-manage-sys/src/main/java/cn/kmbeast/service/impl/BookOrderHistoryServiceImpl.java
@@ -1,92 +1,45 @@
 package cn.kmbeast.service.impl;
 
-import cn.kmbeast.context.LocalThreadHolder;
-import cn.kmbeast.mapper.BookMapper;
 import cn.kmbeast.mapper.BookOrderHistoryMapper;
 import cn.kmbeast.pojo.api.ApiResult;
 import cn.kmbeast.pojo.api.PageResult;
 import cn.kmbeast.pojo.api.Result;
 import cn.kmbeast.pojo.dto.query.extend.BookOrderHistoryQueryDto;
-import cn.kmbeast.pojo.dto.query.extend.BookQueryDto;
-import cn.kmbeast.pojo.entity.Book;
 import cn.kmbeast.pojo.entity.BookOrderHistory;
 import cn.kmbeast.pojo.vo.BookOrderHistoryVO;
-import cn.kmbeast.pojo.vo.BookVO;
 import cn.kmbeast.service.BookOrderHistoryService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 /**
- * 书籍预约历史业务逻辑实现
+ * 书籍借阅历史业务逻辑实现
  */
 @Service
 public class BookOrderHistoryServiceImpl implements BookOrderHistoryService {
 
     @Resource
     private BookOrderHistoryMapper bookOrderHistoryMapper;
-    @Resource
-    private BookMapper bookMapper;
 
     /**
-     * 书籍预约历史新增（借书）
+     * 保存书籍借阅历史
      *
      * @param bookOrderHistory 参数
      * @return Result<Void>
      */
     @Override
     public Result<Void> save(BookOrderHistory bookOrderHistory) {
-        if (bookOrderHistory.getDeadlineNum() == null){
-            return ApiResult.error("请输入本数");
+        if (bookOrderHistory.getCreateTime() == null) {
+            bookOrderHistory.setCreateTime(LocalDateTime.now());
         }
-        if(bookOrderHistory.getReturnTime() == null){
-            return ApiResult.error("请选择归还日期");
-        }
-        LocalDate returnTime = bookOrderHistory.getReturnTime();
-        LocalDate nowDate = LocalDate.now();
-        if (returnTime.isBefore(nowDate)){
-            return ApiResult.error("时间不可倒流哦!");
-        }
-        // 是不是已经借了这本书
-        BookOrderHistoryQueryDto dto = new BookOrderHistoryQueryDto();
-        dto.setUserId(LocalThreadHolder.getUserId());
-        dto.setBookId(bookOrderHistory.getBookId());
-        List<BookOrderHistoryVO> bookOrderHistoryVOS = bookOrderHistoryMapper.query(dto);
-        for (BookOrderHistoryVO bookOrderHistoryVO : bookOrderHistoryVOS) {
-            if (!bookOrderHistoryVO.getIsReturn()) {
-                return ApiResult.error("该书籍未归还");
-            }
-        }
-        // 借的书，是不是有足够的数量
-        BookQueryDto bookQueryDto = new BookQueryDto();
-        bookQueryDto.setId(bookOrderHistory.getBookId());
-        List<BookVO> bookVOS = bookMapper.query(bookQueryDto);
-        if (bookVOS.isEmpty()) {
-            return ApiResult.error("书籍信息找不到");
-        }
-        BookVO bookVO = bookVOS.get(0);
-        if (bookVO.getNum() - bookOrderHistory.getDeadlineNum() < 0) {
-            return ApiResult.error("数量不足");
-        }
-        Book book = new Book();
-        book.setId(bookVO.getId());
-        book.setNum(bookVO.getNum() - bookOrderHistory.getDeadlineNum());
-        // 更新书籍库存
-        bookMapper.update(book);
-        // 设置用户ID
-        bookOrderHistory.setUserId(LocalThreadHolder.getUserId());
-        // 未归还
-        bookOrderHistory.setIsReturn(false);
-        bookOrderHistory.setCreateTime(LocalDateTime.now());
         bookOrderHistoryMapper.save(bookOrderHistory);
         return ApiResult.success();
     }
 
     /**
-     * 书籍预约历史删除
+     * 批量删除书籍借阅历史
      *
      * @param ids 参数
      * @return Result<Void>
@@ -98,44 +51,27 @@ public class BookOrderHistoryServiceImpl implements BookOrderHistoryService {
     }
 
     /**
-     * 书籍预约历史修改（还书）
+     * 更新书籍借阅历史
      *
      * @param bookOrderHistory 参数
      * @return Result<Void>
      */
     @Override
     public Result<Void> update(BookOrderHistory bookOrderHistory) {
-        BookOrderHistoryQueryDto queryDto = new BookOrderHistoryQueryDto();
-        queryDto.setId(bookOrderHistory.getId());
-        // 不能依赖前端传进来的参数，以数据库存储的为准
-        List<BookOrderHistoryVO> bookOrderHistoryVOS = bookOrderHistoryMapper.query(queryDto);
-        // 将书籍的库存加回去
-        BookQueryDto bookQueryDto = new BookQueryDto();
-        bookQueryDto.setId(bookOrderHistoryVOS.get(0).getBookId());
-        List<BookVO> bookVOS = bookMapper.query(bookQueryDto);
-        BookVO bookVO = bookVOS.get(0);
-        Book book = new Book();
-        book.setId(bookOrderHistoryVOS.get(0).getBookId());
-        book.setNum(bookVO.getNum() + bookOrderHistoryVOS.get(0).getDeadlineNum());
-        bookMapper.update(book);
-        // 确认归还
-        bookOrderHistory.setIsReturn(true);
         bookOrderHistoryMapper.update(bookOrderHistory);
         return ApiResult.success();
     }
 
     /**
-     * 书籍预约历史查询
+     * 书籍借阅历史查询
      *
-     * @param dto 查询参数
-     * @return Result<List < BookOrderHistoryVO>>
+     * @param bookOrderHistoryQueryDto 查询参数
+     * @return Result<List<BookOrderHistoryVO>>
      */
     @Override
-    public Result<List<BookOrderHistoryVO>> query(BookOrderHistoryQueryDto dto) {
-        List<BookOrderHistoryVO> noticeList = bookOrderHistoryMapper.query(dto);
-        Integer totalCount = bookOrderHistoryMapper.queryCount(dto);
-        return PageResult.success(noticeList, totalCount);
+    public Result<List<BookOrderHistoryVO>> query(BookOrderHistoryQueryDto bookOrderHistoryQueryDto) {
+        List<BookOrderHistoryVO> historyList = bookOrderHistoryMapper.query(bookOrderHistoryQueryDto);
+        Integer totalCount = bookOrderHistoryMapper.queryCount(bookOrderHistoryQueryDto);
+        return PageResult.success(historyList, totalCount);
     }
-
-
 }
--- books-manage-sys/src/main/java/cn/kmbeast/service/impl/ReaderProposalServiceImpl.java
+++ books-manage-sys/src/main/java/cn/kmbeast/service/impl/ReaderProposalServiceImpl.java
@@ -2,11 +2,14 @@ package cn.kmbeast.service.impl;
 
 import cn.kmbeast.context.LocalThreadHolder;
 import cn.kmbeast.mapper.ReaderProposalMapper;
+import cn.kmbeast.mapper.UserMapper;
 import cn.kmbeast.pojo.api.ApiResult;
 import cn.kmbeast.pojo.api.PageResult;
 import cn.kmbeast.pojo.api.Result;
 import cn.kmbeast.pojo.dto.query.extend.ReaderProposalQueryDto;
+import cn.kmbeast.pojo.dto.query.extend.UserQueryDto;
 import cn.kmbeast.pojo.entity.ReaderProposal;
+import cn.kmbeast.pojo.entity.User;
 import cn.kmbeast.pojo.vo.ReaderProposalVO;
 import cn.kmbeast.service.ReaderProposalService;
 import org.springframework.stereotype.Service;
@@ -23,6 +26,8 @@ public class ReaderProposalServiceImpl implements ReaderProposalService {
 
     @Resource
     private ReaderProposalMapper readerProposalMapper;
+    @Resource
+    private UserMapper userMapper;
 
     /**
      * 读者建议新增
@@ -32,6 +37,14 @@ public class ReaderProposalServiceImpl implements ReaderProposalService {
      */
     @Override
     public Result<Void> save(ReaderProposal readerProposal) {
+        // 如果用户已经被禁言，不能留言
+        UserQueryDto userQueryDto = new UserQueryDto();
+        userQueryDto.setId(LocalThreadHolder.getUserId());
+        List<User> userList = userMapper.query(userQueryDto);
+        User user = userList.get(0);
+        if (user.getIsWord()) {
+            return ApiResult.error("账号已被禁言，请联系管理员");
+        }
         readerProposal.setUserId(LocalThreadHolder.getUserId());
         readerProposal.setIsReply(false);
         readerProposal.setCreateTime(LocalDateTime.now());
@@ -76,11 +89,8 @@ public class ReaderProposalServiceImpl implements ReaderProposalService {
      */
     @Override
     public Result<List<ReaderProposalVO>> query(ReaderProposalQueryDto readerProposalQueryDto) {
-
         List<ReaderProposalVO> noticeList = readerProposalMapper.query(readerProposalQueryDto);
         Integer totalCount = readerProposalMapper.queryCount(readerProposalQueryDto);
         return PageResult.success(noticeList, totalCount);
     }
-
-
 }
